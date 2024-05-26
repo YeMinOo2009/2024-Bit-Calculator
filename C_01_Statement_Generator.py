d9e7bde13c14a76894ee97e2eb9422454d4e97b5
YeMinOo2009 <169116375+YeMinOo2009@users.noreply.github.com>
2024-05-27 09:38:18 +1200

    Delete C_01_Statement_Generator.py

--- C_01_Statement_Generator.py
+++
@@ -1,7 +0,0 @@
-# functions go here 1 usage
-def statement_generator(statement, decoration):
-    print(f"\n{decoration * 5} {statement} {decoration * 5}")
-
-
-# Main routine goes here
-statement_generator("instructions", "-")
